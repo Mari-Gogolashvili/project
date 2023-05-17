52709b55e3fd30757dbe9fcdb03b08ecf7218dea
Mariam Gogolashvili <m_gogolashvili3@cu.edu.ge>
2023-05-17 21:08:47 +0400

    .

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
   function change() {
     var image = document.querySelector(".menu");
     if (image.src.endsWith("icon-menu.svg")) {
-      image.src = "./images/icon-menu-close.svg";
+      image.src = "./styles/images/icon-menu-close.svg";
     } else if (image.src.endsWith("icon-menu-close.svg")) {
-      image.src = "./images/icon-menu.svg";
+      image.src = "/styles/images/icon-menu.svg";
     }
   }
