2b91575150d8d7a8251beffad8bdc9acd19aff50
Mariam Gogolashvili <m_gogolashvili3@cu.edu.ge>
2023-05-17 21:24:15 +0400

    add alt

--- script.js
+++ script.js
@@ -3,6 +3,6 @@
     if (image.src.endsWith("icon-menu.svg")) {
       image.src = "./styles/images/icon-menu-close.svg";
     } else if (image.src.endsWith("icon-menu-close.svg")) {
-      image.src = "/styles/images/icon-menu.svg";
+      image.src = "./styles/images/icon-menu.svg";
     }
   }
